c6484c503302e80a978ecb0e6e86ded4baba8595
Ignacio Jonas <ignacio.jonas@4-hills.com>
2016-06-09 12:38:30 -0300

    Resource Servers Endpoint

--- lib/auth0/api/v2/resource_servers.rb
+++ lib/auth0/api/v2/resource_servers.rb
@@ -1,7 +1,7 @@
 module Auth0
   module Api
     module V2
-      # Methods to use the rules endpoints
+      # Methods to use the resource servers endpoints
       module ResourceServers
         attr_reader :resource_servers_path
 
@@ -16,7 +16,7 @@ module Auth0
           get(path)
         end
 
-        alias_method :get_resource_server, :resource_server
+        alias get_resource_server resource_server
 
         # Creates a new resource server according to the JSON object received in body.
         # @see https://auth0.com/docs/api/management/v2#!/Resource_Servers/post_resource_servers
@@ -26,26 +26,19 @@ module Auth0
         # @param signing_alg [string] The algorithm used to sign tokens.
         # @param signing_secret [string] The secret used to sign tokens when using symmetric algorithms
         # @param token_lifetime [integer] The amount of time (in seconds) that the token will be valid after being issued
-        # @param scope [array] The scope of the resource server.
+        # @param scopes [array] The scope of the resource server.
         #
         # @return [json] Returns the resource server.
-        def create_resource_server(identifier, name=nil, signing_alg = nil, signing_secret=nil, token_lifetime=nil, scope=nil)
-          fail Auth0::InvalidParameter, 'Must supply a valid name' if name.to_s.empty?
-          fail Auth0::InvalidParameter, 'Must supply a valid identifier' if identifier.to_s.empty?
-          request_params = {
-            name: name,
-            identifier: identifier,
-            signing_alg: signing_alg,
-            signing_secret: signing_secret,
-            token_lifetime: token_lifetime,
-            scope: scope,
-          }
+        def create_resource_server(identifier, options = {})
+          fail Auth0::InvalidParameter, 'Must supply a valid resource server id' if identifier.to_s.empty?
+          request_params = Hash[options.map { |(k, v)| [k.to_sym, v] }]
+          request_params[:identifier] = identifier
           post(resource_servers_path, request_params)
         end
 
-        # Deletes a rule.
-        # @see https://auth0.com/docs/api/v2#!/Rules/delete_rules_by_id
-        # @param rule_id [string] The id of the rule to retrieve
+        # Deletes a resource server  by its ID.
+        # @see https://auth0.com/docs/api/management/v2#!/Resource_Servers/delete_resource_servers_by_id
+        # @param resource_server_id [string] The id of the resource server to delete
         def delete_resource_server(resource_server_id)
           fail Auth0::InvalidParameter, 'Must supply a valid resource server id' if resource_server_id.to_s.empty?
           path = "#{resource_servers_path}/#{resource_server_id}"
@@ -56,7 +49,7 @@ module Auth0
 
         # Resource Servers API path
         def resource_servers_path
-          @rules_path ||= '/api/v2/resource_servers'
+          @rules_path ||= '/api/v2/resource-servers'
         end
       end
     end
--- spec/integration/lib/auth0/api/v2/api_resource_servers_spec.rb
+++ spec/integration/lib/auth0/api/v2/api_resource_servers_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 describe Auth0::Api::V2::ResourceServers do
-  attr_reader :client, :enabled_rule, :disabled_rule
+  attr_reader :client, :resource_server
 
   before(:all) do
     @client = Auth0Client.new(v2_creds)
-    name = Faker::Lorem.word
     identifier = Faker::Lorem.word
-    @resource_server = client.create_resource_server(name, identifier)
+    @resource_server = client.create_resource_server(identifier)
   end
 
   after(:all) do
@@ -16,36 +15,33 @@ describe Auth0::Api::V2::ResourceServers do
   describe '.resource_server' do
     it do
       expect(client.resource_server(resource_server['id'])).to(
-        include('name' => resource_server['name'], 'identifier' => resource_server['identifier'], 'id' => resource_server['id']))
+        include('identifier' => resource_server['identifier'], 'id' => resource_server['id'],
+                'signing_alg' => resource_server['signing_alg'],
+                'token_lifetime' => resource_server['token_lifetime']))
     end
   end
 
   describe '.create_resource_server' do
     let(:name) { Faker::Lorem.word }
     let(:identifier) { Faker::Lorem.word }
-    let(:signing_alg) { "HS256" }
-    let(:signing_secret) { Faker::Lorem.word }
-    let(:token_lifetime) { rand(1000..3000)  }
-    let(:scope) { ["Test Scope"] }
+    let(:signing_alg) { 'HS256' }
+    let(:signing_secret) { Faker::Lorem.characters(16) }
+    let(:token_lifetime) { rand(1000..3000) }
     let!(:resource_server) do
-      client.create_resource_server(name, identifier, 'signing_alg' => signing_alg,
-                                                      'signing_secret' => signing_secret,
-                                                      'token_lifetime' => token_lifetime,
-                                                      'scope' => scope) }
+      client.create_resource_server(identifier, 'name' => name, 'signing_alg' => signing_alg,
+                                                'signing_secret' => signing_secret,
+                                                'token_lifetime' => token_lifetime)
     end
-
     it do
       expect(resource_server).to include('name' => name, 'identifier' => identifier, 'signing_alg' => signing_alg,
-                                                                                     'signing_secret' => signing_secret,
-                                                                                     'token_lifetime' => token_lifetime,
-                                                                                     'scope' => scope)
+                                         'signing_secret' => signing_secret,
+                                         'token_lifetime' => token_lifetime)
     end
     it { expect { client.delete_resource_server(resource_server['id']) }.to_not raise_error }
   end
 
   describe '.delete_resource_server' do
     it { expect { client.delete_resource_server(resource_server['id']) }.to_not raise_error }
-    it { expect { client.delete_delete_resource_server '' }.to raise_error(Auth0::InvalidParameter) }
+    it { expect { client.delete_resource_server '' }.to raise_error(Auth0::InvalidParameter) }
   end
-
 end
