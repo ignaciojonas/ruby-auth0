a56b97025ab583b9042a486a9a610076c5f4f0c3
Ignacio Jonas <ignacio.jonas@4-hills.com>
2016-06-09 12:55:08 -0300

    Add User Blocks endpoints

---
+++ spec/integration/lib/auth0/api/v2/api_user_blocks_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+describe Auth0::Api::V2::UserBlocks do
+  attr_reader :client, :user, :email
+
+  before(:all) do
+    @client = Auth0Client.new(v2_creds)
+    username = Faker::Internet.user_name
+    @email = "#{entity_suffix}#{Faker::Internet.safe_email(username)}"
+    password = Faker::Internet.password
+    @user = client.create_user(username,  'email' => email,
+                                          'password' => password,
+                                          'email_verified' => true,
+                                          'connection' => Auth0::Api::AuthenticationEndpoints::UP_AUTH,
+                                          'app_metadata' => {})
+    100.times do
+      begin
+        client.login(email, 'invalid password')
+      rescue Auth0::Unauthorized
+        next
+      rescue Auth0::Unsupported
+        break
+      end
+    end
+  end
+
+  after(:all) do
+    client.delete_user(user['user_id'])
+  end
+
+  describe '.user_blocks' do
+    let(:user_blocks) { client.user_blocks(email) }
+    it { expect(user_blocks['blocked_for'].first['identifier']).to eq email }
+  end
+
+  describe '.user_blocks_by_id' do
+    let(:user_blocks) { client.user_blocks_by_id(user['user_id']) }
+    it { expect(user_blocks['blocked_for'].first['identifier']).to eq email }
+  end
+
+  describe '.delete_user_blocks' do
+    let(:user_blocks) do
+      client.delete_user_blocks(email)
+      client.user_blocks(email)
+    end
+    it { expect(user_blocks['blocked_for']).to eq [] }
+  end
+
+  describe '.delete_user_blocks_by_id' do
+    let(:user_blocks) do
+      client.delete_user_blocks_by_id(user['user_id'])
+      client.user_blocks(email)
+    end
+    it { expect(user_blocks['blocked_for']).to eq [] }
+  end
+end
