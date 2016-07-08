de237827c252fba47c6787151e839c604db46887
Leonardo Soubeste <leonardo.soubeste@4-hills.com>
2016-07-08 17:23:48 -0300

    Add authentication integration tests for endpoints: saml_metadata, wsfed_metadata, token_info, delegation, unlink_user.

--- spec/integration/lib/auth0/api/api_authentication_spec.rb
+++ spec/integration/lib/auth0/api/api_authentication_spec.rb
@@ -23,7 +23,7 @@ describe Auth0::Api::AuthenticationEndpoints do
                                                                     Auth0::Api::AuthenticationEndpoints::UP_AUTH,
                                                                    'app_metadata' => {})
 
-     @global_client = Auth0Client.new(v1_global_creds)
+    @global_client = Auth0Client.new(v1_global_creds)
   end
 
   after(:all) do
@@ -32,26 +32,20 @@ describe Auth0::Api::AuthenticationEndpoints do
   end
 
   describe '.obtain_access_token' do
-    let(:acces_token) do
-      global_client.obtain_access_token
-    end
+    let(:acces_token) { global_client.obtain_access_token }
     it { expect(acces_token).to_not be_nil }
   end
 
   describe '.login' do
-    let(:login) do
-      global_client.login(impersonate_user['email'], password)
-    end
-    it { expect(login).to( include('id_token', 'access_token', 'token_type')) }
+    let(:login) { global_client.login(impersonate_user['email'], password) }
+    it { expect(login).to(include('id_token', 'access_token', 'token_type')) }
   end
 
   describe '.signup' do
     let(:signup_username) { Faker::Internet.user_name }
     let(:signup_email) { "#{entity_suffix}#{Faker::Internet.safe_email(signup_username)}" }
-    let(:signup) do
-      global_client.signup(signup_email, password)
-    end
-    it { expect(signup).to( include('_id', 'email')) }
+    let(:signup) { global_client.signup(signup_email, password) }
+    it { expect(signup).to(include('_id', 'email')) }
   end
 
   describe '.change_password' do
@@ -61,26 +55,70 @@ describe Auth0::Api::AuthenticationEndpoints do
     it { expect(change_password).to eq '"We\'ve just sent you an email to reset your password."' }
   end
 
-  describe '.start_passwordless_email_flow' do
+  skip '.start_passwordless_email_flow' do
     let(:start_passwordless_email_flow) do
       global_client.start_passwordless_email_flow(impersonate_user['email'])
     end
     it { expect(start_passwordless_email_flow).to eq '"We\'ve just sent you an email to reset your password."' }
   end
 
-  describe '.start_passwordless_sms_flow' do
+  skip '.start_passwordless_sms_flow' do
     let(:phone_number) { '+123456778' }
-    let(:start_passwordless_sms_flow) do
-      binding.pry
-      global_client.start_passwordless_sms_flow(phone_number)
-    end
+    let(:start_passwordless_sms_flow) { global_client.start_passwordless_sms_flow(phone_number) }
     it { expect(start_passwordless_sms_flow).to eq '"We\'ve just sent you an email to reset your password."' }
   end
 
-  skip '.impersonation' do
+  describe '.saml_metadata' do
+    let(:saml_metadata) { global_client.saml_metadata }
+    it { expect(saml_metadata).to(include('<EntityDescriptor')) }
+  end
+
+  describe '.wsfed_metadata' do
+    let(:wsfed_metadata) { global_client.wsfed_metadata }
+    it { expect(wsfed_metadata).to(include('<EntityDescriptor')) }
+  end
+
+  describe '.token_info' do
+    let(:id_token) { global_client.login(impersonate_user['email'], password)['id_token'] }
+    let(:token_info) { global_client.token_info(id_token) }
+    it { expect(token_info).to(include('email', 'clientID', 'global_client_id')) }
+  end
+
+  skip '.refresh_delegation' do
+    let(:access_token) { global_client.login(impersonate_user['email'], password)['access_token'] }
+    let(:target) { global_client.clients[0]['clientID'] }
+    let(:refresh_delegation) { global_client.refresh_delegation(access_token, target) }
+    it { expect(refresh_delegation).to(include('email', 'clientID', 'global_client_id')) }
+  end
+
+  describe '.delegation' do
+    let(:id_token) { global_client.login(impersonate_user['email'], password)['id_token'] }
+    let(:target) { global_client.clients[0]['clientID'] }
+    let(:delegation) { global_client.delegation(id_token, target) }
+    it { expect(delegation).to(include('token_type', 'expires_in', 'id_token')) }
+  end
+
+  describe '.impersonation' do
     let(:impersonate_url) do
       global_client.impersonate(impersonate_user['user_id'], ENV['CLIENT_ID'], impersonator_user['user_id'], {})
     end
     it { expect(impersonate_url).to_not be_nil }
+  end
+
+  describe '.unlink_user' do
+    let(:access_token) { global_client.login(impersonate_user['email'], password)['access_token'] }
+    let(:unlink_user) { global_client.unlink_user(access_token, impersonator_user['user_id']) }
+    it { expect(unlink_user).to eq 'OK' }
+  end
+
+  skip '.user_info' do
+    let(:user_info) { global_client.user_info }
+    it { expect(user_info).to eq 'OK' }
+  end
+
+  skip '.authorization_url' do
+    let(:uri) { 'new_uri' }
+    let(:authorization_url) { global_client.authorization_url(uri) }
+    it { expect(get(authorization_url)).to eq 'OK' }
   end
 end
